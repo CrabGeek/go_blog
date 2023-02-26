e8679b6ace2dee33dea123cc83b45c7ed59894d1
guipeng.xie <guipeng.xie@kingstarfintech.com>
2023-02-26 21:16:14 +0800

    移除 URL 路径中的斜杠

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -44,6 +45,15 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// route := http.NewServeMux()
 	route := mux.NewRouter()
@@ -66,5 +76,5 @@ func main() {
 	articleURL, _ := route.Get("articles.show").URL("id", "23")
 	fmt.Println("articleURL: ", articleURL)
 
-	http.ListenAndServe(":3000", route)
+	http.ListenAndServe(":3000", removeTrailingSlash(route))
 }
